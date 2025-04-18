76ea581d3b7db7a90fee2f1b5890c10d7855390a
vinimibielli <vinimibielli@gmail.com>
2025-04-18 00:49:01 -0300

    just comments

--- srcs/include/StartUp.h
+++ srcs/include/StartUp.h
@@ -19,20 +19,18 @@ class StartUp{
 
         PossibleEvents* Events;
 
+        //métodos para inicializar as StartUps
         StartUp();
         ~StartUp();
-
         StartUp(std::string Name, std::string Slogan, int Foundation);
 
+        //métodos de leitura e escrita dos atributos privados
         void setName(std::string Name);
         std::string  getName();
-
         void setSlogan(std::string Slogan);
         std::string  getSlogan();
-
         void setFoundation(int Foundation);
         int  getFoundation();
-
         void setPoints(int Points);
         int  getPoints();
         void editPoints(int changePoints);
